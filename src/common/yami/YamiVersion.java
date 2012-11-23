a6355a7339f98e1d6a8ff43399e468036d9d485a
ohadshai@gmail.com <ohadshai@gmail.com>
2012-11-24 01:12:42 +0200

    add version

---
+++ src/common/yami/YamiVersion.java
@@ -0,0 +1,9 @@
+package yami;
+
+public class YamiVersion
+{
+	public static String major = "0";
+	public static String minor = "1";
+	public static String build = "1";
+	public static String date = "1";
+}
