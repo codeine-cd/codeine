89b02f7395aab782b825967d4494b5c3e4f44f22
yshabi <yaron.shb@gmail.com>
2012-11-29 14:59:16 +0200

    changed hashCode and equals

--- src/common/yami/configuration/GlobalConfiguration.java
+++ src/common/yami/configuration/GlobalConfiguration.java
@@ -7,19 +7,18 @@ public class GlobalConfiguration
 	public String clientpath = "/tmp/yami.monitor/";
 	public int serverport = 8080;
 	public int clientrport = 8112;
-	
 	@Override
 	public int hashCode()
 	{
 		final int prime = 31;
 		int result = 1;
-		result = result * prime  + ((rsync == null) ? 0 : rsync.hashCode());
-		result = result * prime  + ((perl == null) ? 0 : perl.hashCode());
-		result = result * prime  + 8080;
-		result = result * prime  + 8112;
+		result = prime * result + ((clientpath == null) ? 0 : clientpath.hashCode());
+		result = prime * result + clientrport;
+		result = prime * result + ((perl == null) ? 0 : perl.hashCode());
+		result = prime * result + ((rsync == null) ? 0 : rsync.hashCode());
+		result = prime * result + serverport;
 		return result;
 	}
-	
 	@Override
 	public boolean equals(Object obj)
 	{
@@ -30,6 +29,22 @@ public class GlobalConfiguration
 		if (getClass() != obj.getClass())
 			return false;
 		GlobalConfiguration other = (GlobalConfiguration)obj;
+		if (clientpath == null)
+		{
+			if (other.clientpath != null)
+				return false;
+		}
+		else if (!clientpath.equals(other.clientpath))
+			return false;
+		if (clientrport != other.clientrport)
+			return false;
+		if (perl == null)
+		{
+			if (other.perl != null)
+				return false;
+		}
+		else if (!perl.equals(other.perl))
+			return false;
 		if (rsync == null)
 		{
 			if (other.rsync != null)
@@ -37,6 +52,11 @@ public class GlobalConfiguration
 		}
 		else if (!rsync.equals(other.rsync))
 			return false;
+		if (serverport != other.serverport)
+			return false;
 		return true;
 	}
+	
+
+	
 }
