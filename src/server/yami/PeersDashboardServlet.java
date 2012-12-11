71f41d59ca2f0ceebb5aca7ee25f00cb5b521bd2
yshabi <yaron.shb@gmail.com>
2012-12-11 17:15:28 +0200

    added peers control page to allow remote restart of already running peers (<server address>/peers)

---
+++ src/server/yami/PeersDashboardServlet.java
@@ -0,0 +1,81 @@
+package yami;
+
+import java.io.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import org.apache.log4j.*;
+
+import yami.configuration.*;
+import yami.model.*;
+
+public class PeersDashboardServlet extends HttpServlet
+{
+	private static final Logger log = Logger.getLogger(DashboardServlet.class);
+	private static final long serialVersionUID = 1L;
+	GlobalConfiguration gc;
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
+	{
+		gc = ConfigurationManager.getInstance().getCurrentGlobalConfiguration();
+		String hostname = java.net.InetAddress.getLocalHost().getCanonicalHostName();
+		log.debug("dashboard request");
+		DataStore ds = getDataStore();
+		PrintWriter writer = res.getWriter();
+		writer.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">");
+		writer.println("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
+		writer.println("<head>");
+		writer.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />");
+		// writer.println("<meta http-equiv=\"refresh\" content=\"5\" />");
+		writer.println("<title>YAMI Dashboard - Peers administation</title>");
+		writer.println("<link rel=\"stylesheet\" href=\"../style.css\" type=\"text/css\" />");
+		writer.println("");
+		writer.println("</head>");
+		writer.println("<body>");
+		writer.println("<div id=\"container\">");
+		writer.println("  <div id=\"header\">");
+		writer.println("      <h1><a href=\"/\">yami</a></h1>");
+		writer.println("        <h2>" + YamiVersion.get() + "</h2>");
+		writer.println("        <div class=\"clear\"></div>");
+		writer.println("    </div>");
+		writer.println("    <div id=\"nav\">");
+		writer.println("      <ul>");
+		writer.println("          <li class=\"start\"><a href=\"http://" + hostname + ":" + gc.getServerPort() + Constants.DASHBOARD_CONTEXT + "\">Dashboard</a></li>");
+		writer.println("          <li class=\"last\"><a href=\"http://" + hostname + ":" + gc.getServerPort() + "/server" + "\">Server</a></li>");
+		writer.println("          <li class=\"last\"><a href=\"http://" + hostname + ":" + gc.getServerPort() + "/peers" + "\">Peers</a></li>");
+		writer.println("          <li class=\"last\"><a href=\"http://" + hostname + ":" + gc.getServerPort() + "/nodes" + "\">Nodes</a></li>");
+		writer.println("        </ul>");
+		writer.println("    </div>");
+		writer.println("    <div id=\"body\">");
+		writer.println("    <div id=\"content\">");
+		writer.println("      <h2>Active Monitors</h2>");
+		for (Peer peer : ds.peers())
+		{		
+			String line = "            <alert><div class=\"alertbar\"><ul>";
+			line += "<li><a class=\"" + "nameg" + "\" href=\"" + peer.getPeerLink() + "\">" + peer.name + "</a></li>";
+			line += "<li><a class=\"" + "restartbutton" + "\" title=\"" + "Restart" + "\" href=\"" + peer.getPeerRestartLink() + "\">restart</a></li>";
+			line += "</ul><br style=\"clear:left\"/></div></alert>";
+			writer.println(line);
+		}
+		writer.println("        </div>        ");
+		writer.println("    <div class=\"clear\"></div>");
+		writer.println("    </div>");
+		writer.println("    <div id='footer'>");
+		writer.println("    	<div class='footer-content'>");
+		writer.println("    	</div>");
+		writer.println("    </div>");
+		writer.println("    </div>");
+		writer.println("</div>");
+		writer.println("</body>");
+		writer.println("</html>");
+		writer.close();
+	}
+		
+	private DataStore getDataStore()
+	{
+		return DataStoreRetriever.getD();
+	}
+	
+}
