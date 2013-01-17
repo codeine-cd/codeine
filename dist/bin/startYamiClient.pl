b120ffa69423db30febecb0b4aca2a31e7af4fc2
yshabi <yaron.shb@gmail.com>
2013-01-17 08:32:33 +0200

    Added support for starting peer instance with a --kill_current option (killing only a yami java that's listening on the same port we want to start)

--- dist/bin/startYamiClient.pl
+++ dist/bin/startYamiClient.pl
@@ -1,5 +1,20 @@
 #!/usr/bin/perl
 use strict;
+
+my $kill_current = 0;
+my @ARGV2        = ();
+foreach my $arg (@ARGV)
+{
+  if ( $arg eq "--kill_current" || $arg eq "-kill_current" )
+  {
+    $kill_current = 1;
+    next;
+  }
+  push( @ARGV2, $arg );
+}
+
+print "DEBUG: KILL is ON\n";
+@ARGV = @ARGV2;
 die
 "Usage: startYamiClient <java> <rsync> <rsync user> <client port> <server port> <client install dir> <conf file name>  <rsync source>\n"
   if ( @ARGV < 8 );
@@ -31,9 +46,9 @@ sleep 4;
 &rsync_dist;
 
 # current peer instance should be shutdown by the already running java itself.
-if (0)
+if ($kill_current)
 {
-	&kill_current_instance;
+  &kill_current_instance;
 }
 
 &start_peer_java;
@@ -43,91 +58,93 @@ sleep 4;
 my @pids = &get_running_pids;
 if ( not( scalar(@pids) ) )
 {
-	print
-	  "Failed to start yami client, check $start_log for more information\n";
+  print
+    "Failed to start yami client, check $start_log for more information\n";
 }
 else
 {
-	print "Yami Client started successfully as @pids\n";
+  print "Yami Client started successfully as @pids\n";
 }
 
 ###############################################################################
-#								FUNCTIONS									  #
+#               FUNCTIONS                   #
 ###############################################################################
 
 sub create_fs
 {
-	if ( not -e $client_install )
-	{
-		print "DEBUG: creating dir $client_install\n";
-		`mkdir -p $client_install`;
-	}
-	my $chown_cmd = "chown -R $user:root $client_install";
-	print "DEBUG: running $chown_cmd\n";
-	`$chown_cmd`;
+  if ( not -e $client_install )
+  {
+    print "DEBUG: creating dir $client_install\n";
+    `mkdir -p $client_install`;
+  }
+  my $chown_cmd = "chown -R $user:root $client_install";
+  print "DEBUG: running $chown_cmd\n";
+  `$chown_cmd`;
 }
 
 sub rsync_dist
 {
-	print "DEBUG: will run $rsync_cmd\n";
-	`$rsync_cmd`;
-	warn "SCRIPT ERROR: Failed to run ($rsync_cmd)(exit code $?)\n"  if ($?);
+  print "DEBUG: will run $rsync_cmd\n";
+  `$rsync_cmd`;
+  warn "SCRIPT ERROR: Failed to run ($rsync_cmd)(exit code $?)\n" if ($?);
 }
 
 sub start_peer_java
 {
-	my $java_cmd =
+  my $java_cmd =
 "/usr/bin/nohup $java -Djava.path=$java -Ddebug=true -Drsync.path=$rsync -Dyami.conf=$conf_file -Drsync.user=$user -Dclient.port=$client_port -Drsync.source=$rsync_source -cp $client_install/bin/yami.jar yami.YamiClientBootstrap > $start_log 2>&1 < /dev/null &";
-	print "DEBUG: will run ($java_cmd)\n";
-	`$java_cmd`;
+  print "DEBUG: will run ($java_cmd)\n";
+  `$java_cmd`;
 }
 
 sub kill_current_instance
 {
-	my @pids = &get_running_pids();
-	if ( scalar @pids > 0 )
-	{
-		if ( scalar @pids == 1 )
-		{
-			print "DEBUG: going to kill previous instance $pids[0]\n";
-			kill(9, @pids );
-			sleep 5;
-		}
-		else
-		{
-			print
+  my @pids = &get_running_pids();
+
+  if ( scalar @pids > 0 )
+  {
+    if ( scalar @pids == 1 )
+    {
+      print "DEBUG: going to kill previous instance $pids[0]\n";
+      kill( 9, @pids );
+      sleep 5;
+    }
+    else
+    {
+      print
 "ERROR: found more than 1 previous instance of this yami monitor\n";
-		}
-	}
-	else
-	{
-		print "DEBUG: didn't find previous instances for this monitor\n";
-	}
+    }
+  }
+  else
+  {
+    print "DEBUG: didn't find previous instances for this monitor\n";
+  }
 }
 
 sub get_running_pids
 {
-	my $pid_cmd =
-"$PS -eo pid,cmd | grep java | grep 'yami.YamiClientBootstrap\$'| awk '{print \$1}'";
-	print "DEBUG: will run ($pid_cmd)\n";
-	my @pids = `$pid_cmd`;
-	chomp @pids;
-	return @pids;
+  my $pid_cmd =
+"$PS -eo pid,cmd | grep java | grep 'yami.YamiClientBootstrap'| awk '{print \$1}'";
+  print "DEBUG: will run ($pid_cmd)\n";
+  my @pids = `$pid_cmd`;
+  chomp @pids;
+  my @matching_pids = ();
+  foreach my $pid (@pids)
+  {
+    if ( &is_pid_port_match($pid) )
+    {
+      push(@matching_pids,$pid);
+    }
+  }
+  print "DEBUG: found PIDS: (" . join(" ",@matching_pids) . ")\n";
+  return @matching_pids;
 }
 
-sub is_pid_port_match    # need to finish yshabi
+sub is_pid_port_match
 {
-	my $pid = shift;
-	my @out = `lsof -i 'tcp:$client_port'`;
-	return 0 if ($?);
-	chomp @out;
-	foreach my $line (@out)
-	{
-		my @v = split( /[ \t]+/, $line );
-		return 1
-		  if ( $v[1] =~ m/^$pid$/
-			&& $line =~
-			m/($client_port[ \t]+\(LISTEN\)$)|($hostname:$client_port(->))/ );
-	}
-	return 0;
+  my $pid = shift;
+  my @out = `lsof -p $pid | grep 'TCP \\*:$client_port (LISTEN)'`;
+  chomp @out;
+  return 0 if ( $? || scalar(@out) != 1 );
+  return 1;
 }
