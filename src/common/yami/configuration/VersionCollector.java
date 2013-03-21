6cdc99fbb42b69508b4a90866e82964cd27a17a3
oshai <ohadshai@gmail.com>
2013-03-21 23:49:58 +0200

    add keepalive

--- src/common/yami/configuration/VersionCollector.java
+++ src/common/yami/configuration/VersionCollector.java
@@ -2,17 +2,18 @@ package yami.configuration;
 
 public class VersionCollector extends HttpCollector
 {
-
-    public VersionCollector() {
-	super();
-	this.name = "version";
-	this.includedNodes.add("all");
-    }
-    
-    @Override
-	public boolean hasStatus() 
-    {
-	return false;
-    }
-    
+	
+	public VersionCollector()
+	{
+		super();
+		this.name = "version";
+		this.includedNodes.add("all");
+	}
+	
+	@Override
+	public boolean hasStatus()
+	{
+		return false;
+	}
+	
 }
