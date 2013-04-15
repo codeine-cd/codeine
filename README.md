32484dcf790da50606c5b96f5ceea525a29a3b21
oshai <ohadshai@gmail.com>
2013-04-15 12:01:24 +0300

    add doc

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 yami
 ====
+
+how to install:
+1. clone the repository
+2. dist directory is the root for the application (cd dist)
+3. configuration is at conf/yami.conf.xml (see example in conf/example-conf.xml)
+4. to start server: java -jar bin/yami.jar
+
+features:
+* each monitor is a script in monitors dir. 
+exit status 0 is success and other exit status is failure.
+* monitors/version script will enable version for each node. 
+the script output should be the version
+* bin/resatrtAllPeers script will enable restart of all peers.
+* to switch version: <switchVersionEnabled>true</switchVersionEnabled> in configuration.
+in addition add bin/switch-version.
--- src/peer/yami/CommandNodeServlet.java
+++ src/peer/yami/CommandNodeServlet.java
@@ -26,22 +26,29 @@ public class CommandNodeServlet extends HttpServlet
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
 	{
 		log.info("CommandNodeServlet doGet");
-		PrintWriter writer = res.getWriter();
-		writer.println("Recived CommandNodeServlet request");
-		String nodeName = req.getParameter("node");
-		String command = req.getParameter("command");
-		String version = req.getParameter("version");
-		writer.println("node = " + nodeName);
-		writer.println("command = " + command);
-		writer.println("version = " + version);
-		String dir = Constants.getInstallDir() + Constants.MONITORS_DIR;
-		List<String> cmd = Lists.newArrayList(dir + "/" + command);
-		if (version != null)
+		try
 		{
-		    cmd.add(version);
+			PrintWriter writer = res.getWriter();
+			writer.println("Recived CommandNodeServlet request");
+			String nodeName = req.getParameter("node");
+			String command = req.getParameter("command");
+			String version = req.getParameter("version");
+			writer.println("node = " + nodeName);
+			writer.println("command = " + command);
+			writer.println("version = " + version);
+			String dir = Constants.getInstallDir() + Constants.MONITORS_DIR;
+			List<String> cmd = Lists.newArrayList(dir + "/" + command);
+			if (version != null)
+			{
+			    cmd.add(version);
+			}
+			Result result = ProcessExecuter.execute(cmd);
+			writer.println("output: ");
+			writer.println(result.output);
 		}
-		Result result = ProcessExecuter.execute(cmd);
-		writer.println("output: ");
-		writer.println(result.output);
+		catch (Exception ex)
+		{
+			log.warn("error", ex);
+		}
 	}
 }
