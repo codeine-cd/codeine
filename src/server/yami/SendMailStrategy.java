9bc211f5a0f3d5595720d0b035368b6b0de95bb0
oshai <ohadshai@gmail.com>
2013-04-21 10:01:03 +0300

    add version to mail

--- src/server/yami/SendMailStrategy.java
+++ src/server/yami/SendMailStrategy.java
@@ -5,8 +5,12 @@ import java.util.List;
 import yami.configuration.ConfigurationManager;
 import yami.configuration.HttpCollector;
 import yami.configuration.Node;
+import yami.configuration.VersionCollector;
+import yami.mail.CollectorOnNodeState;
 import yami.mail.SendMailTLS;
 import yami.model.Constants;
+import yami.model.DataStore;
+import yami.model.DataStoreRetriever;
 import yami.model.Result;
 
 public class SendMailStrategy
@@ -15,7 +19,8 @@ public class SendMailStrategy
 	public void mailCollectorResult(List<String> mailingList, HttpCollector c, Node n, Result results)
 	{
 		String successString = results.success() ? "OK" : "FAIL";
-		String subject = "yami monitor '" + c.name + "' on " + n.nick() + " is now " + successString;
+		String version = getVersion(n);
+		String subject = "yami monitor '" + c.name + "' on " + n.nick() + version + " is now " + successString;
 		String content = "Collector current status: " + Constants.CLIENT_LINK.replace(Constants.CLIENT_PORT, ConfigurationManager.getInstance().getCurrentGlobalConfiguration().getClientPort() + "").replace(Constants.NODE_NAME, n.name).replace(Constants.PEER_NAME, n.peer.dnsName()).replace(Constants.COLLECTOR_NAME, c.name) + "\n\n";
 		content += "Dashboard: " + Constants.getServerDashboard() + "\n\n";
 		content += "Collector Output:\n";
@@ -29,5 +34,16 @@ public class SendMailStrategy
 		{
 		    SendMailTLS.mail(subject, content, mailingList, cm.getCurrentGlobalConfiguration().email_configuration);
 		}
+	}
+	
+	private String getVersion(Node node) 
+	{
+		DataStore ds = DataStoreRetriever.getD();
+		CollectorOnNodeState result = ds.getResult(node, new VersionCollector());
+		if (null == result)
+		{
+			return "";
+		}
+		return "version " + result.getLast().output;
 	}
 }
