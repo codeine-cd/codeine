1518672fa64da0ead590b374f4fa2fd2dcac2e4c
oshai <ohadshai@gmail.com>
2013-05-01 16:45:28 +0300

    add checkboxes version

--- dist/http-root/dashboard.js
+++ dist/http-root/dashboard.js
@@ -10,3 +10,19 @@ function commandNode(node, command, link)
 	alert('sending command \'' + command + '\' to node ' + node);
 	window.location =  link;
 }
+
+function switchVersionToCheckedItems()
+{
+	//checkbox_
+	var dateRE = /^checkbox_/;
+	var dates=[],els=document.getElementsByTagName('*');
+	for (var i=els.length;i--;) if (dateRE.test(els[i].id]) dates.push(els[i]);
+	var length = arr.length,
+	var nodes = "";
+	for (var i = 0; i < length; i++) {
+		arr[i].checked;
+		nodes += arr[i].id;
+	}
+	alert('switching nodes ' + nodes);
+	window.location =  "/switchVersionAll?ndoes=" + nodes;
+}
