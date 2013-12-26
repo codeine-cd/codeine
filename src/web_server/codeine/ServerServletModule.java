17f595e42f655085ed5790fb0972c6e650b81299
Roi Ezra <ezraroi@gmail.com>
2013-12-25 22:42:19 -0800

    Monitors statistics data is not in a different servlet make page load faster

--- src/web_server/codeine/ServerServletModule.java
+++ src/web_server/codeine/ServerServletModule.java
@@ -10,6 +10,7 @@ import codeine.servlets.FileGetterServlet;
 import codeine.servlets.api_servlets.CommandExecutorApiServlet;
 import codeine.servlets.api_servlets.CommandLogApiServlet;
 import codeine.servlets.api_servlets.DeleteProjectServlet;
+import codeine.servlets.api_servlets.MonitorsStatisticsApiServlet;
 import codeine.servlets.api_servlets.NodesCommandApiServlet;
 import codeine.servlets.api_servlets.ProjectNodesApiServlet;
 import codeine.servlets.api_servlets.ProjectStatusApiServlet;
@@ -36,6 +37,7 @@ public class ServerServletModule extends CodeineServletModule
 		serveMe(ProjectStatusServlet.class, Constants.PROJECT_STATUS_CONTEXT);
 		serveMe(ProjectStatusApiServlet.class, apiContext(Constants.PROJECT_STATUS_CONTEXT));
 		serveMe(CommandLogApiServlet.class, apiContext(Constants.COMMANDS_LOG_CONTEXT));
+		serveMe(MonitorsStatisticsApiServlet.class, apiContext(Constants.MONITORS_STATISTICS_CONTEXT));
 		serveMe(CommandExecutorApiServlet.class, apiContext(Constants.COMMANDS_EXECUTER_STATUS));
 		serveMe(ProjectNodesServlet.class, Constants.PROJECT_NODES_CONTEXT);
 		serveMe(ConfigureProjectServlet.class, Constants.CONFIGURE_PROJECT_CONTEXT);
