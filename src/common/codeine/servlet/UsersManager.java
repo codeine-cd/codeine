79ce54d61e8d4931559359ed964ce9d45d6d60a4
oshai <ohadshai@gmail.com>
2014-01-05 16:43:14 +0200

    fix error in servlet
    error in servlet
    java.lang.IllegalArgumentException: No such user oshai
    at codeine.servlet.UsersManager.user(UsersManager.java:59)
    at
    codeine.servlets.front_end.UserInfoServlet.doGet(UserInfoServlet.java:33)
    at
    codeine.servlet.AbstractFrontEndServlet.myGet(AbstractFrontEndServlet.java:87)
    at codeine.servlet.AbstractServlet.doGet(AbstractServlet.java:35)
    at
    codeine.servlet.AbstractFrontEndServlet.doGet(AbstractFrontEndServlet.java:61)
    at javax.servlet.http.HttpServlet.service(HttpServlet.java:735)
    at javax.servlet.http.HttpServlet.service(HttpServlet.java:848)
    at
    com.google.inject.servlet.ServletDefinition.doService(ServletDefinition.java:263)
    at
    com.google.inject.servlet.ServletDefinition.service(ServletDefinition.java:178)
    at
    com.google.inject.servlet.ManagedServletPipeline.service(ManagedServletPipeline.java:91)
    at
    com.google.inject.servlet.FilterChainInvocation.doFilter(FilterChainInvocation.java:62)
    at
    com.google.inject.servlet.ManagedFilterPipeline.dispatch(ManagedFilterPipeline.java:118)
    at com.google.inject.servlet.GuiceFilter.doFilter(GuiceFilter.java:113)
    at
    org.eclipse.jetty.servlet.ServletHandler$CachedChain.doFilter(ServletHandler.java:1307)
    at
    org.eclipse.jetty.servlet.ServletHandler.doHandle(ServletHandler.java:453)
    at
    org.eclipse.jetty.server.handler.ScopedHandler.handle(ScopedHandler.java:137)
    at
    org.eclipse.jetty.security.SecurityHandler.handle(SecurityHandler.java:521)
    at
    org.eclipse.jetty.server.session.SessionHandler.doHandle(SessionHandler.java:231)
    at
    org.eclipse.jetty.server.handler.ContextHandler.doHandle(ContextHandler.java:1072)
    at
    org.eclipse.jetty.servlet.ServletHandler.doScope(ServletHandler.java:382)
    at
    org.eclipse.jetty.server.session.SessionHandler.doScope(SessionHandler.java:193)
    at
    org.eclipse.jetty.server.handler.ContextHandler.doScope(ContextHandler.java:1006)
    at
    org.eclipse.jetty.server.handler.ScopedHandler.handle(ScopedHandler.java:135)
    at
    org.eclipse.jetty.server.handler.ContextHandlerCollection.handle(ContextHandlerCollection.java:255)
    at
    org.eclipse.jetty.server.handler.HandlerWrapper.handle(HandlerWrapper.java:116)
    at org.eclipse.jetty.server.Server.handle(Server.java:365)
    at
    org.eclipse.jetty.server.AbstractHttpConnection.handleRequest(AbstractHttpConnection.java:485)
    at
    org.eclipse.jetty.server.AbstractHttpConnection.headerComplete(AbstractHttpConnection.java:926)
    at
    org.eclipse.jetty.server.AbstractHttpConnection$RequestHandler.headerComplete(AbstractHttpConnection.java:988)
    at org.eclipse.jetty.http.HttpParser.parseNext(HttpParser.java:635)
    at org.eclipse.jetty.http.HttpParser.parseAvailable(HttpParser.java:235)
    at
    org.eclipse.jetty.server.AsyncHttpConnection.handle(AsyncHttpConnection.java:82)
    at
    org.eclipse.jetty.io.nio.SelectChannelEndPoint.handle(SelectChannelEndPoint.java:627)
    at
    org.eclipse.jetty.io.nio.SelectChannelEndPoint$1.run(SelectChannelEndPoint.java:51)
    at
    org.eclipse.jetty.util.thread.QueuedThreadPool.runJob(QueuedThreadPool.java:608)
    at
    org.eclipse.jetty.util.thread.QueuedThreadPool$3.run(QueuedThreadPool.java:543)
    at java.lang.Thread.run(Thread.java:722)

--- src/common/codeine/servlet/UsersManager.java
+++ src/common/codeine/servlet/UsersManager.java
@@ -2,21 +2,26 @@ package codeine.servlet;
 
 import javax.inject.Inject;
 
+import org.apache.log4j.Logger;
 import org.eclipse.jetty.security.HashLoginService;
 import org.eclipse.jetty.security.LoginService;
 import org.eclipse.jetty.util.security.Credential;
 
+import codeine.jsons.auth.AuthenticationMethod;
 import codeine.jsons.auth.CodeineUser;
 import codeine.jsons.auth.IdentityConfJson;
+import codeine.jsons.global.GlobalConfigurationJsonStore;
 import codeine.model.Constants;
 import codeine.utils.ExceptionUtils;
 import codeine.utils.JsonFileUtils;
 
 public class UsersManager {
 
+	private static final Logger log = Logger.getLogger(UsersManager.class);
 	private @Inject IdentityConfJson identityConfJson;
 	private @Inject HashLoginService hashLoginService;
 	private @Inject JsonFileUtils jsonFileUtils;
+	private @Inject GlobalConfigurationJsonStore globalConfigurationJsonStore;
 	
 	private void store() {
 		jsonFileUtils.setContent(Constants.getIdentityConfPath(), identityConfJson);
@@ -37,10 +42,11 @@ public class UsersManager {
 		hashLoginService.putUser(name, Credential.getCredential(credentials), new String[] { "user" });
 	}
 
-	public void addUser(String name, String credentials) {
-		identityConfJson.add(name, credentials);
+	public CodeineUser addUser(String name, String credentials) {
+		CodeineUser user = identityConfJson.add(name, credentials);
 		store();
 		putUser(name, credentials);
+		return user;
 	}
 	
 	public CodeineUser userByApiToken(String api_token) {
@@ -52,9 +58,12 @@ public class UsersManager {
 	}
 	
 	public CodeineUser user(String name) {
-		
 		for (CodeineUser user : identityConfJson.entries()) {
 			if (user.username().equals(name)) return user;
+		}
+		if (globalConfigurationJsonStore.get().authentication_method() == AuthenticationMethod.WindowsCredentials) {
+			log.info("creating automaticly user " + name);
+			return addUser(name, "NONE");
 		}
 		throw ExceptionUtils.asUnchecked(new IllegalArgumentException("No such user " + name));
 	}
