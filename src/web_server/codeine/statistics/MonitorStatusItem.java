d4f28e148f5b9f7bb41d31947d4d8648d96488cf
oshai <ohadshai@gmail.com>
2014-01-15 12:31:35 +0200

    fixes #51

--- src/web_server/codeine/statistics/MonitorStatusItem.java
+++ src/web_server/codeine/statistics/MonitorStatusItem.java
@@ -4,12 +4,12 @@ package codeine.statistics;
 @SuppressWarnings("unused")
 public class MonitorStatusItem {
 	private String date;
-	private int success;
+	private int total;
 	private int fail;
 	
 	public MonitorStatusItem(String date, int success, int fail) {
 		this.date = date.toString();
-		this.success = success;
+		this.total = success + fail;
 		this.fail = fail;
 	}
 	
