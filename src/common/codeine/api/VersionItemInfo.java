6eab12a923357e9602ff3e7f052faf7ac09625ad
oshai <ohadshai@gmail.com>
2014-01-27 16:48:07 +0200

    fix #75

--- src/common/codeine/api/VersionItemInfo.java
+++ src/common/codeine/api/VersionItemInfo.java
@@ -5,19 +5,33 @@ public class VersionItemInfo {
 
 	private final String version_label;
 	private final String version_name;
-	private int fail_percent, success_percent, count;
+	private int fail_percent, success_percent, count, total_fail;
+	private int max;
 
-	public VersionItemInfo(String version_label, String version_name, int failPercent, int successPercent, int count) {
+	public VersionItemInfo(String version_label, String version_name, int total_fail, int count, int max) {
 		super();
 		this.version_label = version_label;
 		this.version_name = version_name;
-		this.fail_percent = failPercent;
-		this.success_percent = successPercent;
+		this.total_fail = total_fail;
 		this.count = count;
+		this.max = max;
+		if (max == 0) {
+			this.fail_percent = 0;
+			this.success_percent = 0;
+		} else {
+			this.fail_percent = (int) Math.ceil(total_fail * 100 / (double)max);
+			this.success_percent = (int) Math.ceil((count - total_fail) * 100 / (double)max);
+			if (fail_percent + success_percent > 100){
+				success_percent = 100 - fail_percent;
+			}
+		}
 	}
 
 	public int count() {
 		return count;
+	}
+	public int max() {
+		return max;
 	}
 
 	public String version_label() {
@@ -28,6 +42,9 @@ public class VersionItemInfo {
 		return version_name;
 	}
 
+	public int total_fail() {
+		return total_fail;
+	}
 	public int fail_percent() {
 		return fail_percent;
 	}
--- src/web_server/codeine/version/VersionItemTemplate.java
+++ src/web_server/codeine/version/VersionItemTemplate.java
@@ -7,12 +7,10 @@ import codeine.utils.network.HttpUtils;
 public class VersionItemTemplate extends VersionItemInfo{
 	private String version_name_encoded;
 	private String hide_no_errors;
-	private int total_fail;
 	
 	public VersionItemTemplate(VersionItemInfo input) {
-		super(input.version_label(), input.version_name(), input.fail_percent(), input.success_percent(), input.count());
+		super(input.version_label(), input.version_name(), input.total_fail(), input.count(), input.max());
 		this.version_name_encoded = HttpUtils.encode(input.version_name());
 		this.hide_no_errors = input.fail_percent() == 0 ? "display: none;" : "";
-		this.total_fail = (input.count() * 100) /  input.fail_percent();
 	}
 }
