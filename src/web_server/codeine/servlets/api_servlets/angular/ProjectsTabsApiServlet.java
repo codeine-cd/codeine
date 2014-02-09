a480ee7bd5205bbb671dfb2a3b11c823cfb9295f
ohadshai <ohadshai@gmail.com>
2014-02-10 00:31:19 +0200

    fixed exception if no tabs file

--- src/web_server/codeine/servlets/api_servlets/angular/ProjectsTabsApiServlet.java
+++ src/web_server/codeine/servlets/api_servlets/angular/ProjectsTabsApiServlet.java
@@ -10,8 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 import codeine.model.Constants;
 import codeine.servlet.AbstractServlet;
 import codeine.servlets.front_end.ProjectsTab;
+import codeine.utils.FilesUtils;
 import codeine.utils.TextFileUtils;
 
+import com.google.common.collect.Lists;
 import com.google.common.reflect.TypeToken;
 
 public class ProjectsTabsApiServlet extends AbstractServlet {
@@ -28,8 +30,11 @@ public class ProjectsTabsApiServlet extends AbstractServlet {
 	@SuppressWarnings("serial")
 	protected void myGet(HttpServletRequest request, HttpServletResponse response) {
 		String file = Constants.getViewConfPath();
-		Type listType = new TypeToken<ArrayList<ProjectsTab>>() { }.getType();
-		List<ProjectsTab> projects_tabs = gson().fromJson(TextFileUtils.getContents(file), listType);
+		List<ProjectsTab> projects_tabs = Lists.newArrayList();
+		if (FilesUtils.exists(file)) {
+			Type listType = new TypeToken<ArrayList<ProjectsTab>>() { }.getType();
+			projects_tabs = gson().fromJson(TextFileUtils.getContents(file), listType);
+		}
 		writeResponseJson(response, projects_tabs);
 	}
 
