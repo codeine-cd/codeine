dc56b909c9b87d90a43110eb1b6aa73a06dbcdb4
ohadshai <ohadshai@gmail.com>
2014-02-11 00:19:44 +0200

    issue #112 add enhancments for manage page

--- deployment/http-root/ajs/app/Controllers/manageCodeineCtrl.js
+++ deployment/http-root/ajs/app/Controllers/manageCodeineCtrl.js
@@ -3,7 +3,20 @@ angular.module('codeine').controller('manageCodeineCtrl',['$scope', '$log', 'tab
     $scope.tabsForEditing = angular.copy(tabs);
     $scope.permissionsForEditing = angular.copy(permissions);
     $scope.globalConfigurationForEditing = angular.copy($scope.app.globalConfiguration);
-
+//    $scope.new_username = "";
+    $scope.addUser = function() {
+        $scope.permissionsForEditing.push({username : $scope.new_username});
+//        $scope.new_username = "";
+    };
+    $scope.addTab = function() {
+    	$scope.tabsForEditing.push({"a":["a"]});
+    };
+    $scope.select2Options = {
+//    		createSearchChoice: function(term) {
+//    			return {id: term, text: term};
+//    		},
+    		multiple: true
+    };
 }]);
 
 
