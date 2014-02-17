c583e22cbf864ee42605389a81316a3422372157
ohadshai <ohadshai@gmail.com>
2014-02-17 17:50:39 +0200

    get is not working yet

--- deployment/http-root/ajs/app/Controllers/projectConfigureCtrl.js
+++ deployment/http-root/ajs/app/Controllers/projectConfigureCtrl.js
@@ -2,5 +2,5 @@ angular.module('codeine').controller('projectConfigureCtrl',['$scope', '$log', '
     $scope.app.sideBarFile = "/ajs/partials/menus/projectMenu.html";
     $scope.projectName = $routeParams.project_name;
     $log.debug('projectConfigureCtrl: current project is ' + $scope.projectName);
-    $scope.projectConfigurationForEditing = CodeineService.getProjectConfiguration($scope.projectName);
+//    $scope.projectConfigurationForEditing = CodeineService.getProjectConfiguration($scope.projectName);
 }]);
