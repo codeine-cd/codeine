ac258609706e3c05c73fce737c03e86a41ad17c2
ohadshai <ohadshai@gmail.com>
2014-02-20 01:09:24 +0200

    more configure project content

--- deployment/http-root/ajs/app/Controllers/projectConfigureCtrl.js
+++ deployment/http-root/ajs/app/Controllers/projectConfigureCtrl.js
@@ -3,4 +3,19 @@ angular.module('codeine').controller('projectConfigureCtrl',['$scope', '$log', '
     $scope.projectName = $routeParams.project_name;
     $log.debug('projectConfigureCtrl: current project is ' + $scope.projectName);
     $scope.projectConfigurationForEditing = projectConfigurationForEditing;
+
+    $scope.addNode = function() {
+        $scope.projectConfigurationForEditing.nodes_info.push({});
+    };
+
+    $scope.removeNode = function(index) {
+        $scope.projectConfigurationForEditing.nodes_info.splice(index,1);
+    };
+
+    $scope.select2Options = {
+        'multiple': true,
+        'simple_tags': true,
+        //'tags': $scope.projects ,
+        'tokenSeparators': [",", " "]
+    };
 }]);
