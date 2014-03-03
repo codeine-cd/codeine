2f13ed4e89d83adfc96bd95fbcfa684030f365ab
ohadshai <ohadshai@gmail.com>
2014-03-03 23:58:04 +0200

    fix number of default items

--- deployment/http-root/ajs/app/Controllers/commandHistoryCtrl.js
+++ deployment/http-root/ajs/app/Controllers/commandHistoryCtrl.js
@@ -1,7 +1,7 @@
 angular.module('codeine').controller('commandHistoryCtrl',['$scope', '$rootScope', '$log', '$interval','$routeParams','CodeineService', function($scope,$rootScope,$log,$interval,$routeParams,CodeineService) {
     $scope.projectName = $routeParams.project_name;
     $log.debug('commandHistoryCtrl: created for project ' + $scope.projectName);
-    $scope.limit = 1;
+    $scope.limit = 10;
     var getHistory = function() {
         CodeineService.getProjectCommandHistory($scope.projectName).success(function(data) {
             if (!$scope.history || $scope.history.length !== data.length) {
--- deployment/http-root/ajs/app/Controllers/commandRunningCtrl.js
+++ deployment/http-root/ajs/app/Controllers/commandRunningCtrl.js
@@ -1,7 +1,7 @@
 angular.module('codeine').controller('commandRunningCtrl',['$scope', '$rootScope', '$log', '$interval','$routeParams','CodeineService', function($scope,$rootScope,$log,$interval,$routeParams,CodeineService) {
     $scope.projectName = $routeParams.project_name;
     $log.debug('commandRunningCtrl: created for project ' + $scope.projectName);
-    $scope.limit = 1;
+    $scope.limit = 10;
     var getHistory = function() {
         CodeineService.getRunningCommands().success(function(data) {
             if (!$scope.history || $scope.history.length !== data.length) {
