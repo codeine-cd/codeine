ec90fc0244dd3242dc2e02d7822b3e8b84543383
Roi Ezra <ezraroi@gmail.com>
2014-03-05 15:20:51 +0200

    Improved commands history performence

--- deployment/http-root/ajs/app/Controllers/commandHistoryCtrl.js
+++ deployment/http-root/ajs/app/Controllers/commandHistoryCtrl.js
@@ -1,16 +1,26 @@
 angular.module('codeine').controller('commandHistoryCtrl',['$scope', '$rootScope', '$log', '$interval','$routeParams','CodeineService', function($scope,$rootScope,$log,$interval,$routeParams,CodeineService) {
     $scope.projectName = $routeParams.project_name;
-    $log.debug('commandHistoryCtrl: created for project ' + $scope.projectName);
     $scope.limit = 10;
-    var getHistory = function() {
-        CodeineService.getProjectCommandHistory($scope.projectName).success(function(data) {
-            if (!$scope.history || $scope.history.length !== data.length) {
-                $scope.history = data;
-            }
+
+    CodeineService.getProjectCommandHistory($scope.projectName).success(function(data) {
+        $scope.history = data;
+    });
+
+    setInterval(function() {
+        $.ajax( {
+            type: 'GET',
+            url: '/commands-log_json?project=' + $scope.projectName  ,
+            success: function(response) {
+                if  (($scope.history.length !== response.length) || (angular.toJson($scope.history) !== angular.toJson(response))) {
+                    $scope.$apply(function() {
+                        $scope.history = response;
+                    });
+                }
+            },
+            error:  function(err) {
+                $log.error('commandHistoryCtrl: ' + err);
+            },
+            dataType: 'json'
         });
-    }
-
-    getHistory();
-    $interval(getHistory,5000,0,true);
-
+    },5000);
 }]);
--- deployment/http-root/ajs/app/Controllers/commandRunningCtrl.js
+++ deployment/http-root/ajs/app/Controllers/commandRunningCtrl.js
@@ -1,16 +1,26 @@
 angular.module('codeine').controller('commandRunningCtrl',['$scope', '$rootScope', '$log', '$interval','$routeParams','CodeineService', function($scope,$rootScope,$log,$interval,$routeParams,CodeineService) {
     $scope.projectName = $routeParams.project_name;
-    $log.debug('commandRunningCtrl: created for project ' + $scope.projectName);
     $scope.limit = 10;
-    var getHistory = function() {
-        CodeineService.getRunningCommands().success(function(data) {
-            if (!$scope.history || $scope.history.length !== data.length) {
-                $scope.history = data;
-            }
+
+    CodeineService.getRunningCommands().success(function(data) {
+        $scope.history = data;
+    });
+
+    setInterval(function() {
+        $.ajax( {
+            type: 'GET',
+            url: '/commands-status_json',
+            success: function(response) {
+                if  (($scope.history.length !== response.length) || (angular.toJson($scope.history) !== angular.toJson(response))) {
+                    $scope.$apply(function() {
+                        $scope.history = response;
+                    });
+                }
+            },
+            error:  function(err) {
+                $log.error('commandRunningCtrl: ' + err);
+            },
+            dataType: 'json'
         });
-    }
-
-    getHistory();
-    $interval(getHistory,5000,0,true);
-
+    },5000);
 }]);
