ad83f47f24f6fd02f00c60b3180b2657eb7fa1e8
Roi Ezra <ezraroi@gmail.com>
2014-03-24 17:07:38 +0200

    search box is now working

--- deployment/http-root/ajs/app/Controllers/navbarCtrl.js
+++ deployment/http-root/ajs/app/Controllers/navbarCtrl.js
@@ -1,3 +1,21 @@
-angular.module('codeine').controller('navbarCtrl',['$scope', '$rootScope', '$log', function($scope,$rootScope,$log) {
+angular.module('codeine').controller('navbarCtrl',['$scope','$log','CodeineService','$location', function($scope,$log,CodeineService,$location) {
     $log.debug('navbarCtrl: created');
+
+    $scope.model  = {
+        projectSearch : ''
+    };
+
+    CodeineService.getProjects().success(function(data) {
+        $log.debug('navbarCtrl: got ' + data.length + ' projects from server');
+        $scope.projects = data;
+    })
+
+    $scope.$watch('model.projectSearch', function(value) {
+        if (value) {
+            $log.debug('navbarCtrl: selected project: ' + value);
+            $location.path('/codeine/project/' + value + '/status').replace();
+        }
+        $scope.model.projectSearch = '';
+    });
+
 }]);
