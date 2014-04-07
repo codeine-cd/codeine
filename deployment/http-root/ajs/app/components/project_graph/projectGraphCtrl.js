037a0192ca6a70d47ad406dd25c9dd2677d4c2c7
oshai <ohadshai@gmail.com>
2014-04-07 16:25:49 +0300

    small improvments to graph

--- deployment/http-root/ajs/app/components/project_graph/projectGraphCtrl.js
+++ deployment/http-root/ajs/app/components/project_graph/projectGraphCtrl.js
@@ -2,22 +2,30 @@
 angular.module('codeine').controller('projectGraphCtrl',['$scope', '$log','$filter','$routeParams', 'graphData',
     function($scope, $log, $filter,$routeParams,graphData) {
         $scope.projectName = $routeParams.project_name;
+        $scope.commandsDateToName = {};
         $scope.options = {
             axes: {
-                x: {key: 'x', type: 'date', tooltipFormatter: function(x) { return ' ' + $filter('date')(x,'short') + ' ';}},
+                x: {key: 'x', type: 'date', tooltipFormatter: function(x) {
+                    if (angular.isDefined($scope.commandsDateToName[x.toString()])) {
+                        return '   ' + $scope.commandsDateToName[x.toString()] + '   ';
+                    }
+                    return ' ' + $filter('date')(x,'short') + ' ';
+                }},
                 y: {type: 'linear'}
-                //y2: {type: 'linear'}
             },
             series: [
-                {y: 'total', color: 'green',thickness : '3px', label: 'Total'},
-                {y: 'fail', color: 'red',thickness : '3px', label: 'Fail'},
-                {y: 'command', color: 'steelblue',thickness : '10px', label: 'Commands', type: 'column'},
+                {y: 'total', color: 'green',thickness : '6px', label: 'Total'},
+                {y: 'fail', color: 'red',thickness : '6px', label: 'Fail'},
+                {y: 'command', color: 'steelblue',thickness : '6px', label: 'Commands', type: 'column'},
             ],
             lineMode: "cardinal"
         };
         $scope.data = [];
         for (var i=0 ;i < graphData.length; i++) {
             if (graphData[i].date_long !== 0) {
+                if (graphData[i].command_name !== ''){
+                    $scope.commandsDateToName[new Date(graphData[i].date_long).toString()] = graphData[i].command_name;
+                }
                 $scope.data.push({
                     x: new Date(graphData[i].date_long),
                     fail: parseInt(graphData[i].fail),
