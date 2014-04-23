4bb2f1f44ca370a30ccce1b949edca23462d669f
Shai, Ohad <ohad.shai@intel.com>
2014-04-23 08:24:33 +0000

    changed server release to tar

--- build.pl
+++ build.pl
@@ -25,11 +25,14 @@ print "java is $ENV{JAVA_HOME}\n";#1.7
 print "ant is ant\n";#1.8?
 es("cd $codeineDir ; ant", 1);
 es("rsync -ur $codeineDir/deployment/* dist/");
+es("cd dist/http-root/ajs ; npm install");
+es("cd dist/http-root/ajs ; bower install");
+es("cd dist/http-root/ajs ; grunt");
 #es("rsync -ur deployment/bin/* dist/bin/");
 #es("cp $codeineDir/dist/bin/codeine.jar dist/bin/codeine.jar");
 #es("rsync -ur deployment/project dist/");
 #es("rsync -ur deployment/conf dist/");
-es("echo '$ENV{BUILD_NUMBER}' > dist/build_number.txt");
+es("echo '".getVersionNoDate()."' > dist/build_number.txt");
 my $tar = "codeine_".getVersionNoDate().".tar.gz";
 es("cd dist; tar -czf ../$tar ./*");
 print "tar is ready '$tar' for version $version\n";
@@ -37,6 +40,13 @@ my $zip = "codeine_".getVersionNoDate().".zip";
 es("cd dist; zip -r ../$zip ./*");
 print "zip is ready '$zip' for version $version\n";
 
+if (defined($ENV{RELEASE_AREA}))
+{
+	my $link = $ENV{RELEASE_AREA}."/codeine.latest.tar";
+	es("cp $tar $ENV{RELEASE_AREA}/");
+	es("rm $link");
+	es("ln -s $tar $link");
+}
 unless ($ENV{'release-to-github'} eq "true") 
 {
 	exit(0);
@@ -81,7 +91,7 @@ sub updateVersionFile
 	my $minor = getVersion('minor');
 	my $build = $ENV{BUILD_NUMBER};
 	my $date = getDate();
-	es("echo 'CodeineVersion.build=$build\nCodeineVersion.major=$major\nCodeineVersion.minor=$minor\nCodeineVersion.date=$date\n' > $propertiesFile");
+	es("echo 'CodeineVersion.build=$build\nCodeineVersion.major=$major\nCodeineVersion.minor=$minor\n' > $propertiesFile");
 }
 sub getVersionFull
 {
@@ -89,7 +99,7 @@ sub getVersionFull
 	my $minor = getVersion('minor');
 	my $build = $ENV{BUILD_NUMBER} || getVersion('build');
 	my $date = getDate();
-	my $version = "$major.$minor.$build.$date";	
+	my $version = "$major.$minor.$build";	
 	return $version;
 }
 sub getVersionNoDate
