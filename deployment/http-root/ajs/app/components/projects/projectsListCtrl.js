3db3f3a22c6aacd464ed956d377635fe90dbef4d
oshai <ohadshai@gmail.com>
2014-05-18 16:16:38 +0300

    hide tabs without projects

--- deployment/http-root/ajs/app/components/projects/projectsListCtrl.js
+++ deployment/http-root/ajs/app/components/projects/projectsListCtrl.js
@@ -1,6 +1,12 @@
 'use strict';
-angular.module('codeine').controller('projectsListCtrl',['$scope', '$log', 'projects', 'tabs', function($scope, $log, projects, tabs) {
+angular.module('codeine').controller('projectsListCtrl',['$scope', '$log', 'projects', 'tabs', '$filter', function($scope, $log, projects, tabs, $filter) {
     $scope.tabs = tabs.slice(0);
     $scope.tabs.unshift({name:"main", exp: [".*"]});
     $scope.projects = projects;
+
+    $scope.shouldShowTab = function(){
+        return function(tab){
+            return $filter('projectsFilter')($scope.projects, tab).length > 0;
+        }
+    }
 }]);
