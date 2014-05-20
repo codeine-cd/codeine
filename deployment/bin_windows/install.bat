e2706edb1a4e145970ecd1680fe09843826565f2
ohadshai <ohadshai@gmail.com>
2014-05-20 18:16:05 +0300

    add domain name to properties

--- deployment/bin_windows/install.bat
+++ deployment/bin_windows/install.bat
@@ -1,1 +1,7 @@
-wrapper.exe --name="Codeine" --mode=Main --install javaw -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n -Xmx100M -cp ..\bin\codeine.jar codeine.CodeinePeerBootstrap
+@ECHO OFF
+
+FOR /F "tokens=1* delims=REG_SZ " %%A IN ('REG QUERY HKLM\System\CurrentControlSet\Services\Tcpip\Parameters /v Domain') DO (
+SET CURR_DOMAIN=%%B
+)
+
+wrapper.exe --name="Codeine" --mode=Main --install javaw -Xdebug -DDNS_DOMAIN_NAME=%CURR_DOMAIN% -Xrunjdwp:transport=dt_socket,server=y,suspend=n -Xmx100M -cp ..\bin\codeine.jar codeine.CodeinePeerBootstrap
