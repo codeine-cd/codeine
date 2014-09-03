7f1a2d82c0591154112b9a31ed5414a93e4d1061
oshai <ohadshai@gmail.com>
2014-09-03 16:37:45 +0300

    improved url #10

--- src/common/codeine/servlet/ManageStatisticsCollector.java
+++ src/common/codeine/servlet/ManageStatisticsCollector.java
@@ -1,5 +1,8 @@
 package codeine.servlet;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 
@@ -8,16 +11,25 @@ import codeine.utils.ExceptionUtils;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.Lists;
 
 public class ManageStatisticsCollector {
 
 	Cache<String, String> usersInfo = CacheBuilder.newBuilder().maximumSize(20).build();
 	Cache<String, UrlInfo> urlsInfo = CacheBuilder.newBuilder().maximumSize(20).build();
 	
-	public ManageStatisticsInfo getCollected() {
-		return new ManageStatisticsInfo(usersInfo.asMap().keySet(), urlsInfo.asMap().values());
+	public synchronized ManageStatisticsInfo getCollected() {
+		List<UrlInfo> urls = Lists.newArrayList(urlsInfo.asMap().values());
+		Comparator<UrlInfo> c = new Comparator<ManageStatisticsCollector.UrlInfo>() {
+			@Override
+			public int compare(UrlInfo o1, UrlInfo o2) {
+				return o1.hitCount == o2.hitCount ? o1.path.compareTo(o2.path) : Integer.compare(o1.hitCount, o2.hitCount);
+			}
+		};
+		Collections.sort(urls, c);
+		return new ManageStatisticsInfo(usersInfo.asMap().keySet(), urls);
 	}
-	public void userAccess(IUserWithPermissions user, final String pathInfo) {
+	public synchronized void userAccess(IUserWithPermissions user, final String pathInfo) {
 		usersInfo.put(user.user().username(), user.user().username());
 		Callable<UrlInfo> callable = new Callable<ManageStatisticsCollector.UrlInfo>() {
 			@Override
@@ -33,7 +45,6 @@ public class ManageStatisticsCollector {
 		}
 	}
 	
-	@SuppressWarnings("unused")
 	public static class UrlInfo {
 		public UrlInfo(String pathInfo) {
 			path = pathInfo;
