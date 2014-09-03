fc38103b4b8fe00a3dadc0545fa0d3ef7a6c299b
oshai <ohadshai@gmail.com>
2014-09-03 16:28:54 +0300

    improved url #10

--- src/common/codeine/servlet/ManageStatisticsCollector.java
+++ src/common/codeine/servlet/ManageStatisticsCollector.java
@@ -1,6 +1,10 @@
 package codeine.servlet;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+
 import codeine.permissions.IUserWithPermissions;
+import codeine.utils.ExceptionUtils;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
@@ -8,14 +12,34 @@ import com.google.common.cache.CacheBuilder;
 public class ManageStatisticsCollector {
 
 	Cache<String, String> usersInfo = CacheBuilder.newBuilder().maximumSize(20).build();
-	Cache<String, String> urlsInfo = CacheBuilder.newBuilder().maximumSize(20).build();
+	Cache<String, UrlInfo> urlsInfo = CacheBuilder.newBuilder().maximumSize(20).build();
 	
 	public ManageStatisticsInfo getCollected() {
-		return new ManageStatisticsInfo(usersInfo.asMap().keySet(), urlsInfo.asMap().keySet());
+		return new ManageStatisticsInfo(usersInfo.asMap().keySet(), urlsInfo.asMap().values());
 	}
-	public void userAccess(IUserWithPermissions user, String pathInfo) {
+	public void userAccess(IUserWithPermissions user, final String pathInfo) {
 		usersInfo.put(user.user().username(), user.user().username());
-		urlsInfo.put(pathInfo, pathInfo);
+		Callable<UrlInfo> callable = new Callable<ManageStatisticsCollector.UrlInfo>() {
+			@Override
+			public UrlInfo call() throws Exception {
+				return new UrlInfo(pathInfo);
+			}
+		};
+		try {
+			UrlInfo urlInfo = urlsInfo.get(pathInfo, callable);
+			urlInfo.hitCount++;
+		} catch (ExecutionException e) {
+			throw ExceptionUtils.asUnchecked(e);
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	public static class UrlInfo {
+		public UrlInfo(String pathInfo) {
+			path = pathInfo;
+		}
+		private String path;
+		private int hitCount;
 	}
 
 }
--- src/common/codeine/servlet/ManageStatisticsInfo.java
+++ src/common/codeine/servlet/ManageStatisticsInfo.java
@@ -1,14 +1,17 @@
 package codeine.servlet;
 
+import java.util.Collection;
 import java.util.Set;
+
+import codeine.servlet.ManageStatisticsCollector.UrlInfo;
 
 @SuppressWarnings("unused")
 public class ManageStatisticsInfo {
 	
 	private Set<String> users;
-	private Set<String> urls;
+	private Collection<UrlInfo> urls;
 
-	public ManageStatisticsInfo(Set<String> users, Set<String> urls) {
+	public ManageStatisticsInfo(Set<String> users, Collection<UrlInfo> urls) {
 		this.users = users;
 		this.urls = urls;
 	}
