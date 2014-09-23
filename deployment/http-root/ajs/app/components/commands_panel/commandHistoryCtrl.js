78a280e6ba8b6f716c6ca4524b7bf7c7a877defd
ohadshai <ohadshai@gmail.com>
2014-09-24 00:44:10 +0300

    change route to get params correctly

--- deployment/http-root/ajs/app/components/commands_panel/commandHistoryCtrl.js
+++ deployment/http-root/ajs/app/components/commands_panel/commandHistoryCtrl.js
@@ -1,12 +1,13 @@
 'use strict';
-angular.module('codeine').controller('commandHistoryCtrl',['$scope', '$rootScope', '$log', '$interval','$routeParams','CodeineService','Constants', function($scope,$rootScope,$log,$interval,$routeParams,CodeineService,Constants) {
-    $scope.projectName = $routeParams.project_name;
+angular.module('codeine').controller('commandHistoryCtrl',['$scope', '$rootScope', '$log', '$interval','$routeParams','CodeineService','Constants', '$route', function($scope,$rootScope,$log,$interval,$routeParams,CodeineService,Constants,$route) {
+    $scope.projectName = $route.current.params.project_name;
     $scope.limit = 10;
     $scope.historyUrl = Constants.CODEINE_WEB_SERVER + '/api/commands-log?project=' + encodeURI($scope.projectName);
-    if ($routeParams.node_name !== undefined) {
-        $scope.historyUrl += '&node=' + encodeURI($routeParams.node_name);
+    $log.debug('node name is ' + $route.current.params.node_name);
+    if ($route.current.params.node_name !== undefined) {
+        $scope.historyUrl += '&node=' + encodeURI($route.current.params.node_name);
     }
-    CodeineService.getProjectCommandHistory($scope.projectName, $routeParams.node_name).success(function(data) {
+    CodeineService.getProjectCommandHistory($scope.projectName, $route.current.params.node_name).success(function(data) {
         $scope.history = data;
     });
     var maxUpdatesNotInFocus = 100;
