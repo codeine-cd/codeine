d2d656c5153c0d287a3e41639e50462c0ed451ee
oshai <ohadshai@gmail.com>
2014-10-02 10:52:29 +0300

    change write to socket

--- src/common/codeine/utils/SocketUtils.java
+++ src/common/codeine/utils/SocketUtils.java
@@ -1,7 +1,7 @@
 package codeine.utils;
 
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.OutputStream;
 import java.net.Socket;
 
 import org.apache.log4j.Logger;
@@ -11,13 +11,14 @@ public class SocketUtils {
 	private static final Logger log = Logger.getLogger(SocketUtils.class);
 
 	public static void sendToPort(String hostPort, String value) {
-		Socket socket = null;
-		OutputStreamWriter osw;
 		String[] splittd = hostPort.split(":");
+		Socket socket = null;
 		try {
 			socket = new Socket(splittd[0], Integer.valueOf(splittd[1]));
-			osw = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
-			osw.write(value, 0, value.length());
+			OutputStream out = socket.getOutputStream();
+			out.write(value.getBytes());
+			out.flush();
+			out.close();
 		} catch (Exception e) {
 			throw ExceptionUtils.asUnchecked(e);
 		} finally {
