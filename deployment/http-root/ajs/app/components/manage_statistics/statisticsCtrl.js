ad9638681d006bbd36a34bba4d6ebbd45877fa54
ohadshai <ohadshai@gmail.com>
2014-10-19 17:32:59 +0300

    fixed issue #60

--- deployment/http-root/ajs/app/components/manage_statistics/statisticsCtrl.js
+++ deployment/http-root/ajs/app/components/manage_statistics/statisticsCtrl.js
@@ -2,10 +2,20 @@
     'use strict';
 
     //// JavaScript Code ////
-    function statisticsCtrl($scope, statistics) {
+    function statisticsCtrl($scope, statistics, CodeineService, $log, $location) {
         $scope.statsitics  = statistics;
-    }
 
+        $scope.setViewAs = function() {
+            $scope.app.viewAs = $scope.newViewAs;
+            CodeineService.getSessionInfo().success(function(data) {
+                $log.debug('run: got session info ' + angular.toJson(data));
+                $scope.app.sessionInfo = data;
+                $location.path('/codeine');
+            }).error(function() {
+                $scope.app.viewAs = null;
+            });
+        };
+    }
 
     //// Angular Code ////
     angular.module('codeine').controller('statisticsCtrl',statisticsCtrl);
