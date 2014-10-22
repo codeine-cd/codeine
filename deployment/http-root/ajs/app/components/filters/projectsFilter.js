745d5f55bdb4604a110852bbfba9d07950b8345b
ohadshai <ohadshai@gmail.com>
2014-10-22 21:38:38 +0300

    fixed #62

--- deployment/http-root/ajs/app/components/filters/projectsFilter.js
+++ deployment/http-root/ajs/app/components/filters/projectsFilter.js
@@ -7,7 +7,7 @@
             var res = [];
             for(var i=0; i < projects.length; i++) {
                 for(var j=0; j < selectedTab.exp.length; j++) {
-                    var patt = new RegExp(selectedTab.exp[j],"g");
+                    var patt = new RegExp("^" + selectedTab.exp[j] + "$");
                     if (patt.test(projects[i].name)) {
                         res.push(projects[i]);
                         break;
