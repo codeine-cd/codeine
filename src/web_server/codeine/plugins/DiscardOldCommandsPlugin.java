40ef9bcc1943ddf7f22e947164ee2813766cbca1
ohadshai <ohadshai@gmail.com>
2014-11-04 00:44:11 +0200

    added log

--- src/web_server/codeine/plugins/DiscardOldCommandsPlugin.java
+++ src/web_server/codeine/plugins/DiscardOldCommandsPlugin.java
@@ -45,6 +45,7 @@ public class DiscardOldCommandsPlugin {
 	private void deleteCommands(List<CommandExecutionStatusInfo> commandsToDelete) {
 		for (CommandExecutionStatusInfo commandExecutionStatusInfo : commandsToDelete) {
 			String commandOutputDir = pathHelper.getCommandOutputDir(commandExecutionStatusInfo.project_name(), String.valueOf(commandExecutionStatusInfo.id()));
+			log.info("deleteCommands - deleting " + commandOutputDir);
 			try {
 				FilesUtils.delete(commandOutputDir);
 			} catch (Exception e) {
