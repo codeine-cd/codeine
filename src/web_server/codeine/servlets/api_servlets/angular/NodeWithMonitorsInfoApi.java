a910549b329cd7048bf24a605d149a26e315e4d2
oshai <ohadshai@gmail.com>
2014-12-30 10:20:49 +0200

    issue #27 added collectors info for values in status page

--- src/web_server/codeine/servlets/api_servlets/angular/NodeWithMonitorsInfoApi.java
+++ src/web_server/codeine/servlets/api_servlets/angular/NodeWithMonitorsInfoApi.java
@@ -3,12 +3,17 @@ package codeine.servlets.api_servlets.angular;
 import java.util.List;
 
 import codeine.api.NodeWithMonitorsInfo;
+import codeine.jsons.collectors.CollectorExecutionInfo;
+import codeine.utils.StringUtils;
+
+import com.google.common.collect.Lists;
 
 @SuppressWarnings("unused")
 public class NodeWithMonitorsInfoApi extends NodeWithMonitorsInfo {
 
 	private List<String> failed_monitors;
 	private List<String> failed_collectors;
+	private List<CollectorInfo> collectors_info = Lists.newArrayList();
 	private List<String> ok_monitors;
 	private boolean user_can_command;
 	
@@ -19,5 +24,19 @@ public class NodeWithMonitorsInfoApi extends NodeWithMonitorsInfo {
 		this.failed_collectors = info.failed_collectors();
 		this.ok_monitors = info.ok_monitors();
 		this.user_can_command = user_can_command;
+		for (CollectorExecutionInfo collectorInfo : info.collectors().values()) {
+			if (collectorInfo.isSuccess() && !StringUtils.isEmpty(collectorInfo.value())) {
+				collectors_info.add(new CollectorInfo(collectorInfo.name(), collectorInfo.value()));
+			}
+		}
+	}
+	
+	private static class CollectorInfo {
+		private String name;
+		private String value;
+		public CollectorInfo(String name, String value) {
+			this.name = name;
+			this.value = value;
+		}
 	}
 }
