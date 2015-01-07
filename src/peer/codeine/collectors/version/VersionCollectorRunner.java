a788275243dbf9697fb508447e97a9acb8849a33
oshai <ohadshai@gmail.com>
2015-01-07 14:52:53 +0200

    fixed issue - nodes are reporting all the time
    this happens because version and tags runner are created all the time
    and do not have previous results.

--- src/peer/codeine/collectors/version/VersionCollectorRunner.java
+++ src/peer/codeine/collectors/version/VersionCollectorRunner.java
@@ -9,6 +9,7 @@ import codeine.api.NodeInfo;
 import codeine.collectors.IOneCollectorRunner;
 import codeine.collectors.OneCollectorRunner;
 import codeine.collectors.OneCollectorRunnerFactory;
+import codeine.configuration.IConfigurationManager;
 import codeine.jsons.collectors.CollectorInfo;
 import codeine.jsons.collectors.CollectorInfo.CollectorType;
 import codeine.jsons.peer_status.PeerStatus;
@@ -24,22 +25,33 @@ public class VersionCollectorRunner implements IOneCollectorRunner {
 	
 	@Inject private PeerStatus projectStatusUpdater;
 	@Inject private PeerStatusChangedUpdater peerStatusChangedUpdater;
+	private IConfigurationManager configurationManager;
 	private OneCollectorRunner runner;
 	private ProjectJson project;
+	private String projectName;
 	private NodeInfo node;
 
 	@Inject 
-	public VersionCollectorRunner(@Assisted ProjectJson project, @Assisted NodeInfo node,
-			OneCollectorRunnerFactory oneCollectorRunnerFactory) {
+	public VersionCollectorRunner(@Assisted String projectName, @Assisted NodeInfo node,
+			OneCollectorRunnerFactory oneCollectorRunnerFactory, IConfigurationManager configurationManager) {
 		super();
-		this.project = project;
+		this.projectName = projectName;
 		this.node = node;
-		CollectorInfo collectorInfo = new CollectorInfo(Constants.VERSION_COLLECTOR_NAME, project.version_detection_script(), CollectorType.String);
+		this.configurationManager = configurationManager;
+		CollectorInfo collectorInfo = initAndGetConf();
 		runner = oneCollectorRunnerFactory.create(collectorInfo, project, node);
+	}
+
+	private CollectorInfo initAndGetConf() {
+		project = configurationManager.getProjectForName(projectName);
+		CollectorInfo collectorInfo = new CollectorInfo(Constants.VERSION_COLLECTOR_NAME, project.version_detection_script(), CollectorType.String);
+		return collectorInfo;
 	}
 
 	@Override
 	public void execute() {
+		CollectorInfo collectorInfo = initAndGetConf();
+		runner.updateConf(collectorInfo);
 		runner.execute();
 		String version = runner.outputFromFile();
 		log.info("version is " + version);
