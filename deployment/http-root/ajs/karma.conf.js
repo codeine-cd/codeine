305e0bb7294e708c792139b9c54874dfcdcecf6e
Roi Ezra <ezraroi@gmail.com>
2015-07-02 13:55:06 +0300

    Improved client build process

--- deployment/http-root/ajs/karma.conf.js
+++ deployment/http-root/ajs/karma.conf.js
@@ -1,29 +1,60 @@
-module.exports = function(config){
+module.exports = function(config) {
     config.set({
-        basePath : 'app/',
+
+        basePath: './',
 
         files : [
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/es5-shim/es5-shim.js',
-            'bower_components/angular/angular.js',
-            'bower_components/angular-animate/angular-animate.js',
-            'bower_components/angular-mocks/angular-mocks.js',
-            'bower_components/json3/lib/json3.min.js',
-            'bower_components/bootstrap/dist/js/bootstrap.js',
-            'bower_components/angular-route/angular-route.js',
-            'bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js',
-            'bower_components/select2/select2.js',
-            'bower_components/angular-ui-select2/src/select2.js',
-            'bower_components/angular-animate/angular-animate.js',
-            'bower_components/ngstorage/ngStorage.js',
-            'bower_components/angular-ui-utils/ui-utils.js',
-
-            'components/**/*.js',
-            'components/*.js',
-            '../test/**/*.js',
-            '../test/*.js'
+            // bower:
+            'app/bower_components/jquery/dist/jquery.js',
+            'app/bower_components/es5-shim/es5-shim.js',
+            'app/bower_components/json3/lib/json3.js',
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-animate/angular-animate.js',
+            'app/bower_components/angular-route/angular-route.js',
+            'app/bower_components/angular-ui-utils/ui-utils.js',
+            'app/bower_components/select2/select2.js',
+            'app/bower_components/angular-ui-select2/src/select2.js',
+            'app/bower_components/bootstrap/dist/js/bootstrap.js',
+            'app/bower_components/ngstorage/ngStorage.js',
+            'app/bower_components/ng-textcomplete/ng-textcomplete.js',
+            'app/bower_components/underscore/underscore.js',
+            'app/bower_components/ngDistinctValues/ng-distinct-values.js',
+            'app/bower_components/d3/d3.js',
+            'app/bower_components/moment/moment.js',
+            'app/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+            'app/bower_components/waypoints/waypoints.js',
+            'app/bower_components/SHA-1/sha1.js',
+            'app/bower_components/angulartics/src/angulartics.js',
+            'app/bower_components/angulartics/src/angulartics-adobe.js',
+            'app/bower_components/angulartics/src/angulartics-chartbeat.js',
+            'app/bower_components/angulartics/src/angulartics-cnzz.js',
+            'app/bower_components/angulartics/src/angulartics-flurry.js',
+            'app/bower_components/angulartics/src/angulartics-ga-cordova.js',
+            'app/bower_components/angulartics/src/angulartics-ga.js',
+            'app/bower_components/angulartics/src/angulartics-gtm.js',
+            'app/bower_components/angulartics/src/angulartics-kissmetrics.js',
+            'app/bower_components/angulartics/src/angulartics-mixpanel.js',
+            'app/bower_components/angulartics/src/angulartics-piwik.js',
+            'app/bower_components/angulartics/src/angulartics-scroll.js',
+            'app/bower_components/angulartics/src/angulartics-segmentio.js',
+            'app/bower_components/angulartics/src/angulartics-splunk.js',
+            'app/bower_components/angulartics/src/angulartics-woopra.js',
+            'app/bower_components/angulartics/src/angulartics-marketo.js',
+            'app/bower_components/angulartics/src/angulartics-intercom.js',
+            'app/bower_components/angular-duration-filter-formatter/src/duration.js',
+            'app/bower_components/angular-busy/dist/angular-busy.js',
+            'app/bower_components/bootstrap-switch/dist/js/bootstrap-switch.js',
+            'app/bower_components/angular-bootstrap-switch/dist/angular-bootstrap-switch.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'app/bower_components/angular-scenario/angular-scenario.js',
+            // endbower
+            'app/components/**/*.module.js',
+            'app/components/**/*.js',
+            'test/**/*.spec.*js'
         ],
 
+        colors: true,
+        port : 9877,
         autoWatch : true,
 
         frameworks: ['jasmine'],
@@ -31,25 +62,20 @@ module.exports = function(config){
         browsers : ['PhantomJS'],
 
         plugins : [
-            'karma-junit-reporter',
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
             'karma-jasmine',
             'karma-coverage',
             'karma-phantomjs-launcher'
         ],
 
-        junitReporter : {
-            outputFile: 'test_out/unit.xml',
-            suite: 'unit'
-        },
-
         preprocessors : {
             'app/components/**/*.js' : ['coverage']
         },
 
-        reporters : ['coverage']
+        reporters : ['progress', 'coverage'],
 
-        //logLevel: config.LOG_DEBUG
-
-    })}
+        coverageReporter: {
+            type : 'html',
+            dir : 'coverage/'
+        }
+    });
+};
