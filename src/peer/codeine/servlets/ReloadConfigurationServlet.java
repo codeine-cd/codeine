039b8b8b0f468dc3690d3845132006481884c0bd
ezraroi <ezraroi@gmail.com>
2015-10-19 14:34:29 +0300

    fixed #174 - Added sleep and runs in threadpool

--- src/peer/codeine/servlets/ReloadConfigurationServlet.java
+++ src/peer/codeine/servlets/ReloadConfigurationServlet.java
@@ -1,18 +1,23 @@
 package codeine.servlets;
 
 import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import codeine.executer.ThreadPoolUtils;
 import org.apache.log4j.Logger;
 
 import codeine.configuration.IConfigurationManager;
 import codeine.nodes.NodesRunner;
 
 import com.google.inject.Inject;
+import sun.nio.ch.ThreadPool;
 
 public class ReloadConfigurationServlet extends HttpServlet {
 	private static final Logger log = Logger.getLogger(ReloadConfigurationServlet.class);
@@ -21,12 +26,27 @@ public class ReloadConfigurationServlet extends HttpServlet {
 	private NodesRunner nodesRunner;
 	@Inject
 	private IConfigurationManager configurationManager;
+	private ExecutorService threadPool = ThreadPoolUtils.newThreadPool(1, "ReloadConfigurationServlet");
 
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		log.info("ReloadConfigurationServlet started");
-		configurationManager.refresh();
-		nodesRunner.run();
+		log.info("ReloadConfigurationServlet called");
+		Runnable thread = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					int millis = new Random().nextInt((int) TimeUnit.MINUTES.toMillis(1));
+					log.info("ReloadConfigurationServlet going to sleep " + millis);
+					Thread.sleep(millis);
+				} catch (InterruptedException e) {
+					log.error(e);
+				}
+				configurationManager.refresh();
+				nodesRunner.run();
+				log.info("ReloadConfigurationServlet async finished");
+			}
+		};
+		threadPool.submit(thread);
 		res.getWriter().print("{result:'OK'}");
 	}
 }
