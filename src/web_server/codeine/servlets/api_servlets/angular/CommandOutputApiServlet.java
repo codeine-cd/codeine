04b483ba1802889edad276e5817867d1a7cb1d04
rezra3 <roi.ezra@intel.com>
2016-03-29 14:28:54 +0300

    fixed #190 added /command-output to the api that returns the output of the

--- src/web_server/codeine/servlets/api_servlets/angular/CommandOutputApiServlet.java
+++ src/web_server/codeine/servlets/api_servlets/angular/CommandOutputApiServlet.java
@@ -11,11 +11,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
 
 public class CommandOutputApiServlet extends AbstractApiServlet {
 
     private static final long serialVersionUID = 1L;
     private static final Logger log = Logger.getLogger(CommandStatusApiServlet.class);
+    private ExecutorService executorService = Executors.newFixedThreadPool(10);
+
     @Inject
     private PathHelper pathHelper;
 
@@ -25,17 +31,29 @@ public class CommandOutputApiServlet extends AbstractApiServlet {
     }
 
     @Override
-    protected void myGet(HttpServletRequest request, HttpServletResponse response) {
-        String projectName = getParameter(request, Constants.UrlParameters.PROJECT_NAME);
-        String commandID = getParameter(request, Constants.UrlParameters.COMMAND_ID);
+    protected void myGet(final HttpServletRequest request, final HttpServletResponse response) {
+        final String projectName = getParameter(request, Constants.UrlParameters.PROJECT_NAME);
+        final String commandID = getParameter(request, Constants.UrlParameters.COMMAND_ID);
         log.info("Will return command " + commandID + " of project " + projectName + " output");
         request.startAsync();
-        String outfile = pathHelper.getCommandOutputFile(projectName, commandID);
-        try {
-            ByteStreams.copy(new FileInputStream(outfile), request.getAsyncContext().getResponse().getOutputStream());
-        } catch (IOException e) {
-            log.error("Failed to copy stream", e);
-            writeResponseJson(response, e);
-        }
+        final String outfile = pathHelper.getCommandOutputFile(projectName, commandID);
+        FutureTask futureProcess = new FutureTask<>(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                try {
+                    ByteStreams.copy(new FileInputStream(outfile), request.getAsyncContext().getResponse().getOutputStream());
+                } catch (IOException e) {
+                    log.error("Failed to copy stream", e);
+                    writeResponseJson(response, e);
+                    return false;
+                }finally {
+                    log.debug("Finished async request for command " + commandID + " of project " + projectName);
+                    request.getAsyncContext().complete();
+                }
+                return true;
+            }
+        });
+        executorService.execute(futureProcess);
+        futureProcess.run();
     }
 }
