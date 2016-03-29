cb84882a02d6e88cf214772eebcb65a531ac9e2e
rezra3 <roi.ezra@intel.com>
2016-03-29 09:39:20 +0300

    fixed #190 added /command-output to the api that returns the output of the command in streaming

--- src/web_server/codeine/ServerServletModule.java
+++ src/web_server/codeine/ServerServletModule.java
@@ -1,45 +1,19 @@
 package codeine;
 
-import static codeine.model.Constants.*;
-
-import javax.servlet.http.HttpServlet;
-
 import codeine.model.Constants;
 import codeine.servlet.CodeineServletModule;
 import codeine.servlets.AngularServlet;
 import codeine.servlets.RootServlet;
-import codeine.servlets.api_servlets.CancelShutdownApiServlet;
-import codeine.servlets.api_servlets.CommandExecutorApiServlet;
-import codeine.servlets.api_servlets.CommandHistoryApiServlet;
-import codeine.servlets.api_servlets.MonitorsStatisticsApiServlet;
-import codeine.servlets.api_servlets.NodesCommandApiServlet;
-import codeine.servlets.api_servlets.NodesCsvApiServlet;
-import codeine.servlets.api_servlets.PrepareForShutdownApiServlet;
-import codeine.servlets.api_servlets.ProjectStatusApiServlet;
-import codeine.servlets.api_servlets.ProjectTagsApiServlet;
-import codeine.servlets.api_servlets.ReporterServlet;
-import codeine.servlets.api_servlets.UpgradeApiServlet;
-import codeine.servlets.api_servlets.angular.CodeineConfigurationApiServlet;
-import codeine.servlets.api_servlets.angular.CodeineExperimentalConfigurationApiServlet;
-import codeine.servlets.api_servlets.angular.CodeinePermissionsApiServlet;
-import codeine.servlets.api_servlets.angular.CollectorStatusApiServlet;
-import codeine.servlets.api_servlets.angular.CommandStatusApiServlet;
-import codeine.servlets.api_servlets.angular.ManageStatisticsInfoApiServlet;
-import codeine.servlets.api_servlets.angular.MonitorStatusApiServlet;
-import codeine.servlets.api_servlets.angular.NodeStatusApiServlet;
-import codeine.servlets.api_servlets.angular.ProjectCommandsApiServlet;
-import codeine.servlets.api_servlets.angular.ProjectConfigurationApiServlet;
-import codeine.servlets.api_servlets.angular.ProjectNodesAliasesApiServlet;
-import codeine.servlets.api_servlets.angular.ProjectNodesApiServlet;
-import codeine.servlets.api_servlets.angular.ProjectStatus2ApiServlet;
-import codeine.servlets.api_servlets.angular.ProjectsTabsApiServlet;
-import codeine.servlets.api_servlets.angular.RuntimeInfoApiServlet;
-import codeine.servlets.api_servlets.angular.UpdateDbApiServlet;
-import codeine.servlets.api_servlets.angular.UserInfoApiServlet;
+import codeine.servlets.api_servlets.*;
+import codeine.servlets.api_servlets.angular.*;
 import codeine.servlets.api_servlets.projects.ProjectsListApiServlet;
 import codeine.users.RegisterServlet;
-
 import com.google.inject.Scopes;
+
+import javax.servlet.http.HttpServlet;
+
+import static codeine.model.Constants.apiContext;
+import static codeine.model.Constants.apiTokenContext;
 
 public class ServerServletModule extends CodeineServletModule
 {
@@ -75,6 +49,7 @@ public class ServerServletModule extends CodeineServletModule
 		serveApi(MonitorStatusApiServlet.class, (Constants.MONITOR_STATUS_CONTEXT));
 		serveApi(CollectorStatusApiServlet.class, (Constants.COLLECTOR_STATUS_CONTEXT));
 		serveApi(CommandStatusApiServlet.class, (Constants.COMMAND_STATUS_CONTEXT));
+		serveApi(CommandOutputApiServlet.class, (Constants.COMMAND_OUTPUT_CONTEXT));
 		serveApi(RuntimeInfoApiServlet.class, (Constants.SESSION_INFO_CONTEXT));
 		serveApi(ProjectsTabsApiServlet.class, (Constants.PROJECTS_TABS_CONTEXT));
 		serveApi(CodeinePermissionsApiServlet.class, (Constants.PERMISSIONS_CONFIGURATION_CONTEXT));
---
+++ src/web_server/codeine/servlets/api_servlets/angular/CommandOutputApiServlet.java
@@ -0,0 +1,40 @@
+package codeine.servlets.api_servlets.angular;
+
+import codeine.configuration.PathHelper;
+import codeine.model.Constants;
+import codeine.servlet.AbstractApiServlet;
+import com.google.common.io.ByteStreams;
+import org.apache.log4j.Logger;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class CommandOutputApiServlet extends AbstractApiServlet {
+
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = Logger.getLogger(CommandStatusApiServlet.class);
+    @Inject
+    private PathHelper pathHelper;
+
+    @Override
+    protected boolean checkPermissions(HttpServletRequest request) {
+        return canReadProject(request);
+    }
+
+    @Override
+    protected void myGet(HttpServletRequest request, HttpServletResponse response) {
+        String projectName = getParameter(request, Constants.UrlParameters.PROJECT_NAME);
+        String commandID = getParameter(request, Constants.UrlParameters.COMMAND_ID);
+        log.info("Will return command " + commandID + " of project " + projectName + " output");
+        String outfile = pathHelper.getCommandOutputFile(projectName, commandID);
+        try {
+            ByteStreams.copy(new FileInputStream(outfile), request.getAsyncContext().getResponse().getOutputStream());
+        } catch (IOException e) {
+            log.error("Failed to copy stream", e);
+            writeResponseJson(response, e);
+        }
+    }
+}
