5f5f25b9e5cfc1ab13d49aeda4d5e0264b376ed4
rezra3 <roi.ezra@intel.com>
2017-05-09 08:15:44 +0300

    changed to 200 Xmx from 100

--- deployment/bin_windows/wrapper_run.bat
+++ deployment/bin_windows/wrapper_run.bat
@@ -1,1 +1,1 @@
-wrapper.exe --name="Codeine" javaw -Xmx100M -cp bin\codeine.jar codeine.CodeinePeerBootstrap
+wrapper.exe --name="Codeine" javaw -Xmx200M -cp bin\codeine.jar codeine.CodeinePeerBootstrap
