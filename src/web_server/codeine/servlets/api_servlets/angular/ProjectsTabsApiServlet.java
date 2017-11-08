895d39137a078819469f6ea37fcc76fdf548c698
rezra3 <roi.ezra@intel.com>
2017-11-08 08:37:06 +0200

    codeine-200 updating tabs should also run codeine conf modify plugin

--- src/web_server/codeine/servlets/api_servlets/angular/ProjectsTabsApiServlet.java
+++ src/web_server/codeine/servlets/api_servlets/angular/ProjectsTabsApiServlet.java
@@ -1,29 +1,29 @@
 package codeine.servlets.api_servlets.angular;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.log4j.Logger;
-
 import codeine.model.Constants;
+import codeine.plugins.CodeineConfModifyPlugin;
+import codeine.plugins.CodeineConfModifyPlugin.Step;
 import codeine.servlet.AbstractApiServlet;
 import codeine.servlets.api_servlets.ProjectsTab;
 import codeine.utils.FilesUtils;
 import codeine.utils.TextFileUtils;
-
 import com.google.common.collect.Lists;
 import com.google.common.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
 
 @SuppressWarnings("serial")
 public class ProjectsTabsApiServlet extends AbstractApiServlet {
 
 	private static final Logger log = Logger.getLogger(ProjectsTabsApiServlet.class);
 	private static final long serialVersionUID = 1L;
+	@Inject	private CodeineConfModifyPlugin codeineConfModifyPlugin;
 	
 	@Override
 	protected boolean checkPermissions(HttpServletRequest request) {
@@ -56,7 +56,9 @@ public class ProjectsTabsApiServlet extends AbstractApiServlet {
 		ArrayList<ProjectsTab> data = readBodyJson(request, listType);
 		
 		log.info("Will update codeine view configuration. New Config is: " + data);
+		codeineConfModifyPlugin.call(Step.pre, getUser(request).user().username());
 		TextFileUtils.setContents(Constants.getViewConfPath(), gson().toJson(data));
+		codeineConfModifyPlugin.call(Step.post, getUser(request).user().username());
 		writeResponseJson(response, data);
 	}
 
